8e999342c78faaca0cd0c38abb7628f78f4947f9
OkilSaber <okil-saber.lakhdari@epitech.eu>
2022-07-28 12:21:45 +0200

    Add hive initialisation

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:my_book/hive/init.dart';
+import 'package:my_book/hive/models/book/book.dart';
 
-void main() {
+void main() async {
+  await hive.init();
   runApp(const MyBook());
 }
 
